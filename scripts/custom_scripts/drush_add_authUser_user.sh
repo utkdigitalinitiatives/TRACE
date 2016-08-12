0289f9ec0d1a5dd37b6c06cb4a83d79ee7bbd7b2
bridger <bdysonsmith@gmail.com>
2016-08-12 16:01:32 -0400

    add script to drush in authUser

---
+++ scripts/custom_scripts/drush_add_authUser_user.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# adds all of the pieces of the authUser user: user, role, and permissions
+
+## check for and create the user, if the user doesn't exist
+drush -r /var/www/drupal/ user-information authUser 2>&1 | grep '\[error\]' && drush -r /var/www/drupal/ user-create authUser --mail="authenticated-person@example.com" --password="authUser" && echo "Created authUser user" || echo "The authUser account already exists"
+
+## check for and create role, if the role doesn't exist
+drush -r /var/www/drupal/ role-list | grep -o 'authUser-role' && echo "authUser-role exists" || drush -r /var/www/drupal/ role-create 'authUser-role'
+
+## add authUser permissions
+declare -a AUTH_USER_PERMS=(
+	"add fedora datastreams" #islandora
+	"ingest fedora objects" #islandora
+	"replace a datastream with new content, preserving version history" #islandora
+)
+
+# iterate over the list of permissions and verify that they're added
+drush_authUser_role_perm_check() {
+	echo "Verifying authUser-role permissions..."
+	for i in "${AUTH_USER_PERMS[@]}"
+	do
+		drush -r /var/www/drupal/ role-add-perm 'authUser-role' "$i"
+	done
+}
+
+drush_authUser_role_perm_check
+
+## assign authUser-role to authUser user
+drush -r /var/www/drupal/ user-add-role 'authUser-role' authUser
