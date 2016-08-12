15ddbc846214ab50af252b273a6322410afb3026
bridger <bdysonsmith@gmail.com>
2016-08-12 12:26:04 -0400

    add conditional to post.sh; will not automatically update the tomcat-user file.

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setup a user for Tomcat Manager
-sed -i '$i<user username="islandora" password="islandora" roles="manager-gui"/>' /etc/tomcat7/tomcat-users.xml
+grep -q 'user username="islandora"' /etc/tomcat7/tomcat-users.xml && echo "islandora tomcat user exists; not modifying tomcat-users" || sed -i '$i<user username="islandora" password="islandora" roles="manager-gui"/>' /etc/tomcat7/tomcat-users.xml
 service tomcat7 restart
 
 # Set correct permissions on sites/default/files
