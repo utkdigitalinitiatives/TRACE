83ac0cc30ccab00935389c646c2763a7ee57a159
bridger <bdysonsmith@gmail.com>
2016-08-18 15:06:08 -0400

    add advisor-role script to custom_scripts [TRAC-76]

---
+++ scripts/custom_scripts/drush_add_advisor_role.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# adds an advisor-role and appropriate permissions
+
+## check for and create the role if it does not exist
+drush -r /var/www/drupal/ role-list | grep -o 'advisor-role' && echo "advisor-role exists" || drush -r /var/www/drupal/ role-create 'advisor-role'
+
+## add advisor-role permissions
+declare -a ADVISOR_PERMS=(
+	"view old datastream versions"
+	"view fedora repository objects"
+)
+
+## iterate over the list of permissions and verify that they're added
+drush_advisor_role_perm_check() {
+	echo "Verifying advisor-role permissions..."
+	for i in "${ADVISOR_PERMS[@]}"
+	do
+		drush -r /var/www/drupal/ role-add-perm 'advisor-role' "$i"
+	done
+}
+
+# execute!
+drush_advisor_role_perm_check
+
+## we aren't automatically assigning this role to a given user right now
