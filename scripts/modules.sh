8681bd289d0e2e392d9a0472ef119cd80c2f0563
pc37utn <pc37@utk.edu>
2016-08-24 14:34:13 -0400

    add DRUPAL_HOME to first script that runs

--- scripts/modules.sh
+++ scripts/modules.sh
@@ -5,6 +5,8 @@ echo "Installing all Islandora Foundation modules"
 
 SHARED_DIR=$1
 
+export DRUPAL_HOME="/var/www/drupal"
+
 if [ -f "$SHARED_DIR/configs/variables" ]; then
   # shellcheck source=/dev/null
   . "$SHARED_DIR"/configs/variables
