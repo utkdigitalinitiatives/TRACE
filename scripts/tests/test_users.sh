f3d60f5b310f1a1c4bf583233e516b2710b9d52b
pc37utn <pc37@utk.edu>
2016-08-24 14:50:46 -0400

    remove test collection-- use sample pdf that already exists

--- scripts/tests/test_users.sh
+++ scripts/tests/test_users.sh
@@ -8,21 +8,10 @@ echo "ingesting sample content"
 wget https://github.com/utkdigitalinitiatives/Islandora_manual/raw/master/modules/tests/test_files/trace_objects_drush.zip
 unzip trace_objects_drush.zip
 
-# before collections are available, ingest test collection object
-# make directory to hold ingtest object
-mkdir tracetestcollection
-cd tracetestcollection
-#make small dc xml file for collection
-echo "<oai_dc:dc xsi:schemaLocation=\"http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd\">
-<dc:title>trace</dc:title>
-</oai_dc:dc>" > trace.xml
-# set up drupal ingest table
-drush -v -u 1 -r /var/www/drupal ibsp --uri=http://localhost --namespace=trace --content_models=islandora:collectionCModel --type=directory --target=/home/vagrant/tracetestcollection --parent=islandora:islandora
-# ingest from table into fedora
-drush -v -u 1 -r /var/www/drupal islandora_batch_ingest --uri=http://localhost
+# before collections are available, ingest into pdf sample collection in islandora
 cd /home/vagrant
 # set up ingest of pdf objects into test collection
-drush -v -u 1 -r /var/www/drupal ibsp --uri=http://localhost --namespace=trace --content_models=islandora:sp_pdf --type=directory --target=/home/vagrant/trace_objects_drush --parent=islandora:trace
+drush -v -u 1 -r /var/www/drupal ibsp --uri=http://localhost --namespace=sp_pdf_collection --content_models=islandora:sp_pdf --type=directory --target=/home/vagrant/trace_objects_drush --parent=islandora:sp_pdf_collection
 # ingest from table into fedora
 drush -v -u 1 -r /var/www/drupal islandora_batch_ingest --uri=http://localhost
 
