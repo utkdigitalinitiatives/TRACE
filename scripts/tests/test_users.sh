e231a384a091bccdddc6654f7227b1a452724505
pc37utn <pc37@utk.edu>
2016-08-29 16:26:45 -0400

    restore download of sample files

--- scripts/tests/test_users.sh
+++ scripts/tests/test_users.sh
@@ -5,16 +5,16 @@ echo "Checking for sample content"
 
 # ingest sample content
 echo "ingesting sample content"
-#sudo rm -f trace_objects_drush.zip
-#sudo rm -f /home/vagrant/trace_objects_drush/*
-#wget https://github.com/utkdigitalinitiatives/Islandora_manual/raw/master/modules/tests/test_files/trace_objects_drush.zip
-#sudo rm -f rmdir trace_objects_drush
-#unzip trace_objects_drush.zip
+sudo rm -f trace_objects_drush.zip
+sudo rm -f /home/vagrant/trace_objects_drush/*
+wget https://github.com/utkdigitalinitiatives/Islandora_manual/raw/master/modules/tests/test_files/scholar_thesis_pdf_dc.zip
+sudo rm -f rmdir trace_objects_drush
+unzip trace_objects_drush.zip
 
 # before collections are available, ingest into pdf sample collection in islandora
 cd /home/vagrant
 # set up ingest of pdf objects into test collection
-#drush -v -u 1 -r /var/www/drupal ibsp --uri=http://localhost --namespace=sp_pdf_collection --content_models=islandora:sp_pdf --type=directory --target=/home/vagrant/trace_objects_drush --parent=islandora:sp_pdf_collection
+#drush -v -u 1 -r /var/www/drupal ibsp --uri=http://localhost --namespace=libraries --content_models=islandora:ir_thesis --type=directory --target=/home/vagrant/trace_objects_drush --parent=islandora:libraries
 # ingest from table into fedora
 #drush -v -u 1 -r /var/www/drupal islandora_batch_ingest --uri=http://localhost
 
