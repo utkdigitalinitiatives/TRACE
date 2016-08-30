ed2b8d517c3c15664cf0a0d84d24209c5f69327b
pc37utn <pc37@utk.edu>
2016-08-30 15:09:32 -0400

    turn on one time ingest of sample data

--- Vagrantfile
+++ Vagrantfile
@@ -42,7 +42,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   end
   config.vm.provision "post", type: "shell", path: "./scripts/post.sh"
   #config.vm.provision "ldap", type: "shell", path: "./scripts/ldap.sh", args: shared_dir, privileged: "false"
-  config.vm.provision "test_users", type: "shell", path: "./scripts/tests/test_users.sh", privileged: "false"
+  config.vm.provision :shell, path: "./scripts/tests/test_users.sh", :args => shared_dir, :privileged => false
   
   if File.exist?("~/Desktop/traceCustomModule") then
     config.vm.synced_folder "~/Desktop/traceCustomModule", "/var/www/drupal/sites/all/modules/traceCustomModule", type: "rsync",
--- scripts/tests/test_users.sh
+++ scripts/tests/test_users.sh
@@ -2,28 +2,18 @@
 #
 echo "Checking for sample content"
 # checking if sample content exists
-if [ -f "./scholar_thesis_pdf_dc.zip" ]; then
-  echo "Deleting previous data file."
-  rm -f ./scholar_thesis_pdf_dc.zip
+if [ ! -f "./scholar_thesis_pdf_dc.zip" ]; then
+  # ingest sample content
+  echo "downloading sample content"
+  wget https://github.com/utkdigitalinitiatives/Islandora_manual/raw/master/modules/tests/test_files/scholar_thesis_pdf_dc.zip
+  echo "Unzipping sample files"
+  unzip scholar_thesis_pdf_dc.zip
+  # ingest into islandora:libraries sample collection in islandora
+  cd /home/vagrant
+  # set up ingest of ir:thesis objects into test collection
+  drush -v -u 1 -r /var/www/drupal ibsp --uri=http://localhost --namespace=libraries --content_models=islandora:sp_pdf --type=directory --target=/home/vagrant/scholar_thesis_pdf_dc --parent=islandora:libraries
+  # ingest from table into fedora
+  drush -v -u 1 -r /var/www/drupal islandora_batch_ingest --uri=http://localhost
 fi
-
-# ingest sample content
-echo "downloading sample content"
-wget https://github.com/utkdigitalinitiatives/Islandora_manual/raw/master/modules/tests/test_files/scholar_thesis_pdf_dc.zip
-if [ -d "scholar_thesis_pdf_dc"]; then
-  echo "Deleteing previous data files and directory"
-  rm -f /home/vagrant/scholar_thesis_pdf_dc/*
-  rmdir ./scholar_thesis_pdf_dc
-fi
-echo "Unzipping sample files"
-unzip scholar_thesis_pdf_dc.zip
-
-# ingest into islandora:libraries sample collection in islandora
-cd /home/vagrant
-# set up ingest of ir:thesis objects into test collection
-#drush -v -u 1 -r /var/www/drupal ibsp --uri=http://localhost --namespace=libraries --content_models=islandora:ir_thesisCModel --type=directory --target=/home/vagrant/scholar_thesis_pdf_dc --parent=islandora:libraries
-# ingest from table into fedora
-#drush -v -u 1 -r /var/www/drupal islandora_batch_ingest --uri=http://localhost
-
 # start user tests
 echo "Testing default users of roles"
