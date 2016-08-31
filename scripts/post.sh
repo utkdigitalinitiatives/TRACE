788c783e0713d276b4174d7ee6725e1634bbfe8a
Don Richards <highlander411@hotmail.com>
2016-08-31 11:58:07 -0400

    Added admin-gui to allow for gui login & Added Tomcat role via Bridger's suggestion

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setup a user for Tomcat Manager
-grep -q 'user username="islandora"' /etc/tomcat7/tomcat-users.xml && echo "islandora tomcat user exists; not modifying tomcat-users" || sed -i '$i<user username="islandora" password="islandora" roles="manager-gui"/>' /etc/tomcat7/tomcat-users.xml
+grep -q 'user username="islandora"' /etc/tomcat7/tomcat-users.xml && echo "islandora tomcat user exists; not modifying tomcat-users" || sed -i '$i<role rolename="tomcat"/>\n<role rolename="admin-gui"/>\n<user username="islandora" password="islandora" roles="tomcat,manager-gui,admin-gui"/>' /etc/tomcat7/tomcat-users.xml
 service tomcat7 restart
 
 # Set correct permissions on sites/default/files
