74632a9c2773f093394632bee9c83e251f9c6bc6
bridger <bdysonsmith@gmail.com>
2016-09-06 11:53:46 -0400

    remove grep/sed for tomcat-users; settings have been corrected in the base box.

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Setup a user for Tomcat Manager
-grep -q 'user username="islandora"' /etc/tomcat7/tomcat-users.xml && echo "islandora tomcat user exists; not modifying tomcat-users" || sed -i '$i<role rolename="tomcat"/>\n<role rolename="admin-gui"/>\n<user username="islandora" password="islandora" roles="tomcat,manager-gui,admin-gui"/>' /etc/tomcat7/tomcat-users.xml
-service tomcat7 restart
-
 # Set correct permissions on sites/default/files
 chmod -R 775 /var/www/drupal/sites/default/files
