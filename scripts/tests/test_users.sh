93e34c7e4370488091d6a5206a52b553d092d716
pc37utn <pc37@utk.edu>
2016-09-07 10:03:14 -0400

    add log file for testing

--- scripts/tests/test_users.sh
+++ scripts/tests/test_users.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 #
 echo "Checking for sample content"
+# create testing log
+touch /home/vagrant/testing.log
+cd /home/vagrant
+echo -e "====Initilize log for pass/fail testing===\n" > ./testing.log
 # checking if sample content exists
 if [ ! -f "./scholar_thesis_pdf_dc.zip" ]; then
   # ingest sample content
-  echo "downloading sample content"
+  echo -e "downloading sample content.....\n"
   wget https://github.com/utkdigitalinitiatives/Islandora_manual/raw/master/modules/tests/test_files/scholar_thesis_pdf_dc.zip
+  # download pass/fail
+  if [ -f "./scholar_thesis_pdf_dc.zip" ]; then
+    echo -e "Pass: Download of scholar_thesis_pdf_dc.zip\n" >> ./testing.log
+  else
+    echo -e "Fail: Download of scholar_thesis_pdf_dc.zip\n" >> ./testing.log
+  fi
   echo "Unzipping sample files"
   unzip scholar_thesis_pdf_dc.zip
   # ingest into islandora:libraries sample collection in islandora
@@ -15,33 +25,45 @@ if [ ! -f "./scholar_thesis_pdf_dc.zip" ]; then
   # ingest from table into fedora
   drush -v -u 1 -r /var/www/drupal islandora_batch_ingest --uri=http://localhost
 fi
+# test if ingest was successful
+curl -s -o libraries_3.pdf http://localhost:8000/islandora/object/libraries%3A3/datastream/OBJ/view#overlay-context=islandora/object/libraries%253A3
+if [ -f ./libraries_3.pdf ]; then
+  echo -e "Pass: ingest of sample data\n" >> ./testing.log
+  rm -f ./libraries_3.pdf
+else
+  echo -e "Fail: ingest of sample data\n" >> ./testing.log
+fi
+
 # start user tests
 echo -e "\nTesting default users of roles\n"
+echo -e "Logging to /home/vagrant/testing.log\n"
 # url of an obj: http://localhost:8000/islandora/object/libraries%3A3/datastream/OBJ/view#overlay-context=islandora/object/libraries%253A3
 #
-#test curl of obj with authUser
-curl -s -o libraries_3.pdf -u authUser:authUser http://localhost:8000/islandora/object/libraries%3A3/datastream/OBJ/view#overlay-context=islandora/object/libraries%253A3
+#test anonymous
+curl -s -o libraries_3.pdf  http://localhost:8000/islandora/object/libraries%3A3/datastream/OBJ/view#overlay-context=islandora/object/libraries%253A3
 if [ -f ./libraries_3.pdf ]; then
-  echo "authuser can download content"
-  rm -f ./libraries_3.pdf
-fi
-if [! -f /home/vagrant/user-curl.sh ]; then
-  cp /vagrant/scripts/tests/user-curl.sh /home/vagrant/
-fi
- echo -e "\nRun the user-curl.sh for the current state of users."
-echo -e "\n***** Testing authUser: "
-curl -s -o libraries_3.pdf -u authUser:authUser http://localhost:8000/islandora/object/libraries%3A3/datastream/OBJ/view#overlay-context=islandora/object/libraries%253A3
-if [ -f ./libraries_3.pdf ]; then
-  echo "authuser can download content"
+  echo -e "Pass: anonymous can download content\n" >> ./testing.log
   rm -f ./libraries_3.pdf
 else
-  echo "authUser cannot download content"
+  echo -e "Fail: anonymous can download content\n" >> ./testing.log
 fi
-echo -e "\n***** Testing manager: "
+#test authuser
+curl -s -o libraries_3.pdf -u authUser:authUser http://localhost:8000/islandora/object/libraries%3A3/datastream/OBJ/view#overlay-context=islandora/object/libraries%253A3
+if [ -f ./libraries_3.pdf ]; then
+  echo -e "Pass: authuser can download content\n" >> ./testing.log
+  rm -f ./libraries_3.pdf
+else
+  echo -e "Fail: authUser can download content\n" >> ./testing.log
+fi
+# test manager
 curl -s -o libraries_3.pdf -u manager:manager http://localhost:8000/islandora/object/libraries%3A3/datastream/OBJ/view#overlay-context=islandora/object/libraries%253A3
 if [ -f ./libraries_3.pdf ]; then
-  echo "manager can download content"
+  echo -e "Pass: manager can download content\n" >> ./testing.log
   rm -f ./libraries_3.pdf
 else
-  echo "manager cannot download content"
+  echo -e "Fail: manager can download content\n" >> ./testing.log
 fi
+if [! -f /home/vagrant/user-curl.sh ]; then
+  sudo  cp /vagrant/scripts/tests/usertest.sh /home/vagrant/
+fi
+ echo -e "Run the usertest.sh for the current state of users\n"
