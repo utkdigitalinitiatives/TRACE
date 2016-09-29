5ff322813ff1c2924a1108cf5195415d901c795a
pc37utn <pc37@utk.edu>
2016-09-29 09:30:40 -0400

    fix download

--- scripts/tests/test_users.sh
+++ scripts/tests/test_users.sh
@@ -9,27 +9,28 @@ echo -e "====Initilize log for pass/fail testing===\n" > ./testing.log
 if [ ! -f "./scholar_thesis_pdf_dc.zip" ]; then
   # ingest sample content
   echo -e "downloading sample content.....\n"
-  wget -q https://github.com/utkdigitalinitiatives/Islandora_manual/raw/master/modules/tests/test_files/scholar_thesis_pdf_dc.zip
+  #wget -q https://github.com/utkdigitalinitiatives/Islandora_manual/raw/master/modules/tests/test_files/scholar_thesis_pdf_dc.zip
+  #wget -q http://dlwork.lib.utk.edu/vboxes/trace_samples/set001/alt_170.zip
   # download pass/fail
-  if [ -f "./scholar_thesis_pdf_dc.zip" ]; then
-    echo -e "Pass: Download of scholar_thesis_pdf_dc.zip\n" >> ./testing.log
+  if [ -f "./alt_170.zip" ]; then
+    echo -e "Pass: Download of alt_170.zip\n" >> ./testing.log
   else
-    echo -e "Fail: Download of scholar_thesis_pdf_dc.zip\n" >> ./testing.log
+    echo -e "Fail: Download of alt_170.zip\n" >> ./testing.log
   fi
   echo "Unzipping sample files"
-  unzip scholar_thesis_pdf_dc.zip
+  unzip alt_170.zip
   # ingest into islandora:libraries sample collection in islandora
   cd /home/vagrant
   # set up ingest of ir:thesis objects into gradthes collection
-  drush -v -u 1 -r /var/www/drupal ibsp --uri=http://localhost --namespace=gradthes --content_models=ir:thesisCModel --type=directory --target=/home/vagrant/scholar_thesis_pdf_dc --parent=islandora:gradthes
+  drush -v -u 1 -r /var/www/drupal ibsp --uri=http://localhost --namespace=gradthes --content_models=ir:thesisCModel --type=directory --target=/home/vagrant/alt_170 --parent=islandora:gradthes
   # ingest from table into fedora
   drush -v -u 1 -r /var/www/drupal islandora_batch_ingest --uri=http://localhost
 fi
 # test if ingest was successful
 curl -s -o libraries_3.pdf http://localhost:8000/islandora/object/libraries%3A3/datastream/OBJ/view#overlay-context=islandora/object/libraries%253A3
-if [ -f ./libraries_3.pdf ]; then
+if [ -f ./libraries_1.pdf ]; then
   echo -e "Pass: ingest of sample data\n" >> ./testing.log
-  rm -f ./libraries_3.pdf
+  rm -f ./libraries_1.pdf
 else
   echo -e "Fail: ingest of sample data\n" >> ./testing.log
 fi
