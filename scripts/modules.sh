810b3fd623c6b61ed3c07e58b29f4b8f4c2972f9
pc37utn <pc37@utk.edu>
2016-10-04 09:00:30 -0400

    remove the marcxml module

--- scripts/modules.sh
+++ scripts/modules.sh
@@ -82,7 +82,7 @@ drush -y -u 1 en islandora_basic_image islandora_large_image islandora_video
 drush -y -u 1 en islandora_checksum islandora_checksum_checker
 drush -y -u 1 en islandora_book_batch islandora_pathauto islandora_pdfjs islandora_videojs islandora_jwplayer islandora_openseadragon
 drush -y -u 1 en xml_forms xml_form_builder xml_schema_api xml_form_elements xml_form_api jquery_update zip_importer islandora_basic_image islandora_bibliography islandora_compound_object islandora_google_scholar islandora_scholar_embargo islandora_solr_config citation_exporter doi_importer endnotexml_importer pmid_importer ris_importer
-drush -y -u 1 en islandora_fits islandora_ocr islandora_oai islandora_marcxml islandora_simple_workflow islandora_xacml_api islandora_xacml_editor islandora_xmlsitemap colorbox islandora_internet_archive_bookreader islandora_bagit islandora_batch_report islandora_usage_stats islandora_populator
+drush -y -u 1 en islandora_fits islandora_ocr islandora_oai islandora_simple_workflow islandora_xacml_api islandora_xacml_editor islandora_xmlsitemap colorbox islandora_internet_archive_bookreader islandora_bagit islandora_batch_report islandora_usage_stats islandora_populator
 drush -y -u 1 en islandora_binary_object
 drush -y -u 1 en islandora_ingest_collections
 
