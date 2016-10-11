a9d7a6ef81b7137859ce2ac6ad1258f4664ddd30
Don Richards <highlander411@hotmail.com>
2016-10-11 10:25:37 -0400

    Shellcheck SC1009 Add # shellcheck source=/dev/null before the if command

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -3,9 +3,10 @@
 # run all shell scripts in scripts/custom_scripts/
 for SCRIPT in /vagrant/scripts/custom_scripts/*.sh
 do
-	if [ -f "$SCRIPT" ]; then
-		echo "Running custom scripts"
-			# shellcheck source=/dev/null
-			source "$SCRIPT"
-	fi
+ # shellcheck source=/dev/null
+ if [ -f "$SCRIPT" ]; then
+  echo "Running custom scripts"
+  # shellcheck source=/dev/null
+  source "$SCRIPT"
+ fi
 done
