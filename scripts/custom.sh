663408f4cde4b6acec02f82fcb554ae7da4d7640
Don Richards <highlander411@hotmail.com>
2016-10-12 14:58:59 -0400

    Debugging Shellcheck

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+shellcheck --version
 # run all shell scripts in scripts/custom_scripts/
+# shellcheck source=/dev/null
 for SCRIPT in /vagrant/scripts/custom_scripts/*.sh
 do
- # shellcheck source=/dev/null
  if [ -f "$SCRIPT" ]; then
   echo "Running custom scripts"
   # shellcheck source=/dev/null
