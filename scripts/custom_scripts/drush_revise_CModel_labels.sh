d0e17353ee1695624caa3e7f4ec48547ebae483e
cdeaneGit <cdeane@utk.edu>
2016-10-12 12:08:23 -0400

    Trac-214 script to change CMODEL Labels.

---
+++ scripts/custom_scripts/drush_revise_CModel_labels.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "drush revise CModel Labels"
+
+#Original Label                         Content Model Type
+#Islandora Basic Image Content Model    islandora:sp_basic_image
+#Islandora PDF Content Model            islandora:sp_pdf
+#Islandora Large Image Content Model    islandora:sp_large_image_cmodel
+#Thesis Content Model                   ir:thesisCModel
+#Islandora Binary Object Content Model  ir:binaryObjectCmodel
+
+cd /usr/local/fedora
+
+# islandora:sp_basic_image
+curl -vv -u "fedoraAdmin:fedoraAdmin" -X PUT "http://localhost:8080/fedora/objects/islandora:sp_basic_image?label=chd_basic_image"
+
+# islandora:sp_pdf
+curl -vv -u "fedoraAdmin:fedoraAdmin" -X PUT "http://localhost:8080/fedora/objects/islandora:sp_pdf?label=chd_pdf"
+
+# islandora:sp_large_image_cmodel
+curl -vv -u "fedoraAdmin:fedoraAdmin" -X PUT "http://localhost:8080/fedora/objects/islandora:sp_large_image_cmodellabel=chd_large_image"
+
+
+#ir:thesisCModel
+curl -vv -u "fedoraAdmin:fedoraAdmin" -X PUT "http://localhost:8080/fedora/objects/ir:thesisCModel?label=chd_thesis"
+
+
+# islandora:binaryObjectCmodel
+curl -vv -u "fedoraAdmin:fedoraAdmin" -X PUT "http://localhost:8080/fedora/objects/islandora:binaryObjectCModel?label=chd_misc"
+
+echo "CModel Labels have been revised."
+
+exit
