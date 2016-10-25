033bf8c079f27f8dbe1f738896b0d0bed7cdc7ef
Robert Patrick Waltz (UTK) <rwaltz@utk.edu>
2016-10-25 17:31:58 -0400

    Changing a lot of code. drush add-menu-item code was not formatted correctly.  changed the menu for navigation so that only the trace repository link is shown. added the bookmarks link to user-menu.

--- scripts/custom_scripts/110_modify_default_menus.sh
+++ scripts/custom_scripts/110_modify_default_menus.sh
@@ -4,30 +4,49 @@
 #currently the values would be system | navigation | 3 ;
 #The role Id should be queried from the role table for the name 'administrator'
 cd "$DRUPAL_HOME"
+#cd "/var/www/drupal"
 
-drush sql-query "insert into block_role (rid, module, delta) select rid, 'system', 'navigation' from role where name like 'administrator'"
+#display the title of the menu as 'Admin Menu' in instead of 'Navigation'
+drush sql-query "update block set title='Admin Menu' where module = 'system' and delta = 'navigation'"
 
-#Create simpler user navigation menu for any other authenticated user
+#limit to administators who can see the 'Admin Menu'
+drush sql-query "insert into block_role (rid, module, delta) select rid, 'system', 'navigation' from role where name = 'administrator'"
+
+#when in the administration menu screen, show the title of the menu as Admin Navigation
+drush sql-query "update menu_custom set title = 'Admin Navigation' where menu_name = 'navigation'"
+
+#Change the display name of 'Islandora Repository' to 'Trace Collections'
+drush sql-query "update menu_links set link_title='Trace Collections' where menu_name = 'navigation' and link_path = 'islandora' and link_title = 'Islandora Repository'"
+
+#The user-menu should only be shown to logged in users, but not administrator (or authenticated)
+drush sql-query "insert into block_role (rid, module, delta) select rid, 'system', 'user-menu' from role where name in ( 'authUser-role', 'manager-role', 'privUser-role')"
+
+#Change the display name of 'Islandora Repository' to 'Trace Collections'
+drush sql-query "update menu_router set title = 'Trace Collections' where path= 'islandora'"
+
+
 #if drush extras does not exist then 
 if [ ! -d $HOME/.drush/drush_extras ]; then
   drush dl drush_extras
 fi
 
-drush sql-query "update menu_custom set title='Admin Navigation' where menu_name like 'navigation'"
-
-#confirm if drush exists
-if drush menu-create menu-default-navigation --title="Navigation" --description="The Default Navigation for authenticated Users"; then
-	drush add-menu-item menu-default-navigation --title="Trace Collections" --path="islandora"
-	drush add-menu-item menu-default-navigation --title="My Bookmarks" --path="islandora-bookmark"
+# add in a new menu that will show a link for navigation to the Trace Collections
+if drush menu-create menu-trace-navigation --title="Trace Navigation" --description="Navigation to the Trace Collections"; then
+	drush add-menu-item menu-trace-navigation "Trace Collections" islandora
+	drush block-configure --theme="UTKdrupal" --module="menu" --delta="menu-trace-navigation" --region="sidebar_first"
+	# do not display the block title because it looks messy
+	drush sql-query "update block set title='<none>' where module = 'menu' and delta = 'menu-trace-navigation'"
+	# do not show this link to administrators since it is duplicate information and is messy
+	drush sql-query "insert into block_role (rid, module, delta) select rid, 'menu', 'menu-trace-navigation' from role where name in ( 'anonymous user', 'authUser-role', 'manager-role', 'privUser-role')"
+#	
 fi
 
-drush sql-query "insert into block_role (rid, module, delta) select rid, 'menu', 'menu-default-navigation' from role where name in ( 'authUser-role', 'manager-role', 'privUser-role')"
 
-drush block-configure --theme=UTKdrupal --module=menu --delta=menu-default-navigation --region=sidebar_first
-drush block-configure --theme=bartik --module=menu --delta=menu-default-navigation --region=sidebar_first
-#The user menu does not show up in any content area .
 #update the block table such that any table row with a module and delta of 'system', 'user-menu' such that the region is 'content'
 
-drush block-configure --theme=UTKdrupal --module=system --delta=user-menu --region=sidebar_first
-drush block-configure --theme=bartik --module=system --delta=user-menu --region=sidebar_first
+#The user menu should show up in the sidebar.
+drush block-configure --theme="UTKdrupal" --module="system" --delta="user-menu" --region="sidebar_first"
+drush add-menu-item user-menu "My Bookmarks" islandora-bookmark
+
+#get rid of the powered by block
 drush block-disable --module=system --delta=powered-by
