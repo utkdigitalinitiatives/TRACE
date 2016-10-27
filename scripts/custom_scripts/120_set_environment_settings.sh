83068e324da21146cfd5488ef8355ab72300ab6f
Don Richards <highlander411@hotmail.com>
2016-10-27 13:48:29 -0400

    Environment Vars set

--- scripts/custom_scripts/120_set_environment_settings.sh
+++ scripts/custom_scripts/120_set_environment_settings.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+# Change permissions to edit
+sudo chmod 777 /var/www/drupal/sites/default/settings.php
+
 # Sets string replacement
 sudo cat /vagrant/files/string_replacement >> /var/www/drupal/sites/default/settings.php
+
+# Change permissions back
+sudo chmod 555 /var/www/drupal/sites/default/settings.php
 
 cd /var/www/drupal || exit
 
 drush vset site_mail dlcontact@utk.edu
 php -r "print json_encode(array('dlcontact@utk.edu'));" | drush vset --format=json update_notify_emails -
-
 drush vset site_default_country US
+drush vset site_frontpage 'islandora/object/islandora%3Aroot'
+drush vset date_default_timezone 'America/New_York'
