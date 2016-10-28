e67cc73a2c4bef0a4004183f3db482f88c7c1241
Don Richards <highlander411@hotmail.com>
2016-10-28 12:41:45 -0400

    Enable PrivateMsg but permissions not set yet

---
+++ scripts/custom_scripts/130_add_private_messaging.sh
@@ -0,0 +1,18 @@
+# This adds Private Messaging to TRACE
+# Installs Privatemsg (https://www.drupal.org/project/privatemsg)
+# & Real Name (https://www.drupal.org/project/realname)
+cd "$DRUPAL_HOME"/ || exit
+
+drush -y dl realname
+drush -y en realname
+
+drush -y dl privatemsg
+drush -y en privatemsg
+drush -y en pm_block_user pm_email_notify privatemsg_filter privatemsg_limits privatemsg_realname privatemsg_roles privatemsg_rules
+
+
+# Set to "Secondary sidebar" blocks
+
+# Set block to be viewable by only authenticated users
+drush dl drush_extras
+drush block-configure --module=privatemsg --delta=privatemsg-menu --region=sidebar_second
