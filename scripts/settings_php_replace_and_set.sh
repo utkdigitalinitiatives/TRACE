9a322b22c1c8ac26a7617e91197fe28cad281035
Don Richards <highlander411@hotmail.com>
2016-10-28 09:45:09 -0400

    Add event log when ran

--- scripts/settings_php_replace_and_set.sh
+++ scripts/settings_php_replace_and_set.sh
@@ -7,3 +7,6 @@
 
 # Sends base_url to settings.php
 sudo bash -c 'cat /vagrant/files/string_replacement >> /var/www/drupal/sites/default/settings.php'
+
+# Log that is ran
+echo "Set settings.php Script Ran" >> ../time.log
