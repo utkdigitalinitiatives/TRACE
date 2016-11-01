c3e067c237e8e3b072bdcef2352503d0a2da9096
Don Richards <highlander411@hotmail.com>
2016-11-01 07:17:55 -0400

    Correct string cat to settings.php

--- scripts/custom_scripts/130_add_private_messaging.sh
+++ scripts/custom_scripts/130_add_private_messaging.sh
@@ -10,9 +10,12 @@ drush -y dl privatemsg
 drush -y en privatemsg
 drush -y en pm_block_user pm_email_notify privatemsg_filter privatemsg_limits privatemsg_realname privatemsg_roles privatemsg_rules
 
-
 # Set to "Secondary sidebar" blocks
 
 # Set block to be viewable by only authenticated users
 drush dl drush_extras
 drush block-configure --module=privatemsg --delta=privatemsg-menu --region=sidebar_second
+
+# Clean Up
+sudo updatedb
+druch cc all
--- scripts/settings_php_replace_and_set.sh
+++ scripts/settings_php_replace_and_set.sh
@@ -6,7 +6,7 @@
  */
 
 # Sends base_url to settings.php
-sudo bash -c 'cat /vagrant/files/string_replacement >> /var/www/drupal/sites/default/settings.php'
+sudo bash -c 'cat /vagrant/files/change_base_url >> /var/www/drupal/sites/default/settings.php'
 
 # Log that is ran
 echo "Set settings.php Script Ran" >> ../time.log
