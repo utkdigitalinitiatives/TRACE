f01bc38b3ce295e2ff1487da9df9de27d0d15764
Robert Patrick Waltz (UTK) <rwaltz@utk.edu>
2016-11-02 12:48:29 -0400

    Changing name of Trace Collections to Collections to conform to front page naming convention, moving some code into another script

--- scripts/custom_scripts/110_modify_default_menus.sh
+++ scripts/custom_scripts/110_modify_default_menus.sh
@@ -4,7 +4,6 @@
 #currently the values would be system | navigation | 3 ;
 #The role Id should be queried from the role table for the name 'administrator'
 cd "$DRUPAL_HOME"
-#cd "/var/www/drupal"
 
 #display the title of the menu as 'Admin Menu' in instead of 'Navigation'
 drush sql-query "update block set title='Admin Menu' where module = 'system' and delta = 'navigation'"
@@ -16,13 +15,13 @@ drush sql-query "insert into block_role (rid, module, delta) select rid, 'system
 drush sql-query "update menu_custom set title = 'Admin Navigation' where menu_name = 'navigation'"
 
 #Change the display name of 'Islandora Repository' to 'Trace Collections'
-drush sql-query "update menu_links set link_title='Trace Collections' where menu_name = 'navigation' and link_path = 'islandora' and link_title = 'Islandora Repository'"
+drush sql-query "update menu_links set link_title='Collections' where menu_name = 'navigation' and link_path = 'islandora' and link_title = 'Islandora Repository'"
 
 #The user-menu should only be shown to logged in users, but not administrator (or authenticated)
 drush sql-query "insert into block_role (rid, module, delta) select rid, 'system', 'user-menu' from role where name in ( 'authUser-role', 'manager-role', 'privUser-role')"
 
 #Change the display name of 'Islandora Repository' to 'Trace Collections'
-drush sql-query "update menu_router set title = 'Trace Collections' where path= 'islandora'"
+drush sql-query "update menu_router set title = 'Collections' where path= 'islandora'"
 
 #if drush extras does not exist then
 if [ ! -d $HOME/.drush/drush_extras ]; then
@@ -31,7 +30,7 @@ fi
 
 # add in a new menu that will show a link for navigation to the Trace Collections
 if drush menu-create menu-trace-navigation --title="Trace Navigation" --description="Navigation to the Trace Collections"; then
-	drush add-menu-item menu-trace-navigation "Trace Collections" islandora
+	drush add-menu-item menu-trace-navigation "Collections" islandora
 	drush block-configure --theme="UTKdrupal" --module="menu" --delta="menu-trace-navigation" --region="sidebar_first"
 	# do not display the block title because it looks messy
 	drush sql-query "update block set title='<none>' where module = 'menu' and delta = 'menu-trace-navigation'"
@@ -39,11 +38,3 @@ if drush menu-create menu-trace-navigation --title="Trace Navigation" --descript
 	drush sql-query "insert into block_role (rid, module, delta) select rid, 'menu', 'menu-trace-navigation' from role where name in ( 'anonymous user', 'authUser-role', 'manager-role', 'privUser-role')"
 fi
 
-#update the block table such that any table row with a module and delta of 'system', 'user-menu' such that the region is 'content'
-
-#The user menu should show up in the sidebar.
-drush block-configure --theme="UTKdrupal" --module="system" --delta="user-menu" --region="sidebar_first"
-drush add-menu-item user-menu "My Bookmarks" islandora-bookmark
-
-#get rid of the powered by block
-drush block-disable --module=system --delta=powered-by
