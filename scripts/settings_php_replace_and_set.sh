c6a4fc5a2d32af05b34518dfb715186b18474863
bridger <bdysonsm@utk.edu>
2016-11-07 15:02:13 -0500

    fix comment section

--- scripts/settings_php_replace_and_set.sh
+++ scripts/settings_php_replace_and_set.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-/**
- * @file for string replacement
- * Concatenate to /var/www/drupal/sites/default/settings.php
- */
+#/**
+# * @file for string replacement
+# * Concatenate to /var/www/drupal/sites/default/settings.php
+# */
 
 # Sends base_url to settings.php
 sudo bash -c 'cat /vagrant/files/change_base_url >> /var/www/drupal/sites/default/settings.php'
