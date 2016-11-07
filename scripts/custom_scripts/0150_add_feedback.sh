e8a04d79356d6a5258b7fc436617fe0758696db8
Don Richards <highlander411@hotmail.com>
2016-11-07 09:07:59 -0500

    Change Feedback to UserVoice

--- scripts/custom_scripts/0150_add_feedback.sh
+++ scripts/custom_scripts/0150_add_feedback.sh
@@ -1,33 +1,47 @@
 #!/usr/bin/env bash
 
-# requires sites/all/libraries/ua_parser/src/ua-parser.js
-cd /var/www/drupal/sites/all/libraries || exit
-
-git clone https://github.com/faisalman/ua-parser-js
-
 cd /var/www/drupal/sites/all/modules || exit
 
-# Download and install with dependencies
-# https://www.drupal.org/project/feedback_collect
-drush -y dl feedback_collect
-drush -y en feedback_collect
+# utklibrary.uservoice.com
 
-# Set the permissions for feedback to display.
-# Used this 'drush role-list 'anonymous user' --format=variables'
-drush role-add-perm 'anonymous user' 'create feedback_collect content'
-drush role-add-perm 'authenticated user' 'create feedback_collect content'
+drush -y dl uservoice
+drush -y en uservoice
 
-# Configure Module settings
-drush -y vset feedback_collect_settings_anonymous 1
-drush -y vset feedback_collect_settings_bootstrap_support false
-drush -y vset feedback_collect_settings_button_text 'Add feedback'
-drush -y vset feedback_collect_settings_custom_css 1
-drush -y vset feedback_collect_settings_email 'DonRichards@utk.edu'
-drush -y vset feedback_collect_settings_exclude 'admin/*'
-drush -y vset feedback_collect_settings_hide_text false
-drush -y vset feedback_collect_settings_icon_classes false
-drush -y vset feedback_collect_settings_margin 50px
-drush -y vset feedback_collect_settings_placement 2
-
-# Content permissions need to be rebuilt at this stage
-drush php-eval 'node_access_rebuild();'
+drush -y vset uservoice_classic_widget_default_mode 'support'
+drush -y vset uservoice_classic_widget_link_color '#007cbf'
+drush -y vset uservoice_classic_widget_mode 'full'
+drush -y vset uservoice_classic_widget_primary_color '#cc6d00'
+drush -y vset uservoice_classic_widget_tab_color '#cc6d00'
+drush -y vset uservoice_classic_widget_tab_inverted: 0
+drush -y vset uservoice_classic_widget_tab_label 'Feedback & Support'
+drush -y vset uservoice_classic_widget_tab_position 'middle-right'
+drush -y vset uservoice_contact_form '0'
+drush -y vset uservoice_js_api_key 'FD76oSQaYThdUhkScrcsQ'
+drush -y vset uservoice_new_widget_accent_color '#FF8200'
+drush -y vset uservoice_new_widget_autopromp '0'
+drush -y vset uservoice_new_widget_autoprompt: 1
+drush -y vset uservoice_new_widget_custom_triggers: 0
+drush -y vset uservoice_new_widget_default_forum ''
+drush -y vset uservoice_new_widget_starting_experience 'contact'
+drush -y vset uservoice_new_widget_trigger_background_color '#FF8200'
+drush -y vset uservoice_new_widget_trigger_color 'rgba(240 237 227, 0.6)'
+drush -y vset uservoice_new_widget_trigger_position 'bottom-right'
+drush -y vset uservoice_script_cache_time '0'
+drush -y vset uservoice_sso_key 'd1bb433dd8ce0f30b5f1202fb6eafc62'
+drush -y vset uservoice_subdomain 'utklibrary'
+drush -y vset uservoice_webhooks: 0
+drush -y vset uservoice_widget_pages 'admin
+admin/*
+batch'
+drush -y vset uservoice_widget_roles: array (
+  1 => '1',
+  2 => '2',
+  3 => '3',
+  4 => '4',
+  5 => '5',
+  6 => '6',
+  7 => '7',
+)
+drush -y vset uservoice_widget_type 'new'
+drush -y vset uservoice_widget_visibility_pages '0'
+drush -y vset uservoice_widget_visibility_roles '0'
