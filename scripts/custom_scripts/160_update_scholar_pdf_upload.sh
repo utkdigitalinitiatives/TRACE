c4aaa17c2b583e0133df6ec92933d306adcabbb5
bridger <bdysonsmith@gmail.com>
2016-11-10 14:04:52 -0500

    update islandora_scholar URL 160_update_scholar shell script

--- scripts/custom_scripts/160_update_scholar_pdf_upload.sh
+++ scripts/custom_scripts/160_update_scholar_pdf_upload.sh
@@ -5,9 +5,13 @@ echo "Checking for an islandora_scholar repository"
 
 cd "$HOME_DIR" || exit
 
+# Committing this with CoB/islandora_scholar as a temporary starting point, until
+# changes are incorporated into utkdigitalinitiatives
 if [ ! -d "$HOME_DIR"/islandora_scholar ]; then
-	echo "Cloning utkdigitalinitiatives/islandora_scholar"
-	git clone https://github.com/utkdigitalinitiatives/islandora_scholar
+	#echo "Cloning utkdigitalinitiatives/islandora_scholar"
+	echo "Cloning CanOfBees/islandora_scholar"
+	#git clone https://github.com/utkdigitalinitiatives/islandora_scholar
+	git clone https://github.com/CanOfBees/islandora_scholar
 	chown -hR vagrant:vagrant islandora_scholar
 else
 	cd islandora_scholar || exit
