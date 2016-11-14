40f9cb643892b769931d869972b3fbe112368e62
Mark Baggett <mbagget1@utk.edu>
2016-11-14 16:05:31 -0500

    Add script to ingest sample records.

---
+++ scripts/custom_scripts/170_ingest_sample_theses.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# cURL Repo
+cd "$HOME_DIR" || exit
+if [ ! -d "$HOME_DIR"/sample_data ]; then
+    echo "cURLing Sample Thesis Data"
+	mkdir sample_data || exit
+	chown -hR vagrant:vagrant sample_data
+	cd sample_data || exit
+	curl -O http://dlwork.lib.utk.edu/trace_sample_data/trace_sample_data.tar.gz
+	tar -xvzf trace_sample_data.tar.gz
+else
+	cd sample_data || exit
+	echo "Updating Example Thesis Records"
+	chown -hR vagrant:vagrant sample_data
+	curl -O http://dlwork.lib.utk.edu/trace_sample_data/trace_sample_data.tar.gz
+	tar -xvzf trace_sample_data.tar.gz
+fi
+
+# Ingest Sample Data
+
+cd "$DRUPAL_HOME" || exit
+echo "Ingesting Sample Thesis Objects"
+drush -v --user=admin --uri=http://localhost ibsp --content_models=ir:thesisCModel --type=directory --parent=utk.ir:td --namespace=utk.ir.td --target=/home/vagrant/sample_data/trace_sample_data
+drush -v --user=admin --uri=http://localhost islandora_batch_ingest
+
