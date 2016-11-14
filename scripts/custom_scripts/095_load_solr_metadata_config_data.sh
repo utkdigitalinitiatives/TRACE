36ae4e39d905dc614daeb3a94c60f625bd108f3c
pc37utn <pc37@utk.edu>
2016-11-14 14:11:46 -0500

    correct spelling

--- scripts/custom_scripts/095_load_solr_metadata_config_data.sh
+++ scripts/custom_scripts/095_load_solr_metadata_config_data.sh
@@ -2,24 +2,24 @@
 
 
 echo "Checking for solr_metadata sql files"
-if [ -f "/vagrant/config/solr-tables/islandora_solr_metadata_cmodels.sql" ]; then
+if [ -f "/vagrant/configs/solr-tables/islandora_solr_metadata_cmodels.sql" ]; then
   echo "Loading islandora_solr_metadata_cmodels.sql"
-	drush sql-cli < /vagrant/config/solr-tables/islandora_solr_metadata_cmodels.sql
+	drush sql-cli < /vagrant/configs/solr-tables/islandora_solr_metadata_cmodels.sql
 	drush cc all
 fi
-if [ -f "/vagrant/config/solr-tables/islandora_solr_metadata_associations.sql" ]; then
+if [ -f "/vagrant/configs/solr-tables/islandora_solr_metadata_associations.sql" ]; then
   echo "Loading islandora_solr_metadata_associations.sql"
-	drush sql-cli < /vagrant/config/solr-tables/islandora_solr_metadata_associations.sql
+	drush sql-cli < /vagrant/configs/solr-tables/islandora_solr_metadata_associations.sql
 	drush cc all
 fi
-if [ -f "/vagrant/config/solr-tables/islandora_solr_metadata_fields.sql" ]; then
+if [ -f "/vagrant/configs/solr-tables/islandora_solr_metadata_fields.sql" ]; then
   echo "Loading islandora_solr_metadata_fields.sql"
-	drush sql-cli < /vagrant/config/solr-tables/islandora_solr_metadata_fields.sql
+	drush sql-cli < /vagrant/configs/solr-tables/islandora_solr_metadata_fields.sql
 	drush cc all
 fi
-if [ -f "/vagrant/config/solr-tables/islandora_solr_fields.sql" ]; then
+if [ -f "/vagrant/configs/solr-tables/islandora_solr_fields.sql" ]; then
   echo "Loading islandora_solr_fields.sql"
-	drush sql-cli < /vagrant/config/solr-tables/islandora_solr_fields.sql
+	drush sql-cli < /vagrant/configs/solr-tables/islandora_solr_fields.sql
 	drush cc all
 fi
 
