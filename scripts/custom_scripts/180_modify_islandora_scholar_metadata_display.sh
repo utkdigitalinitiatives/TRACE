8fc0992c57e5aa78b6579c46125a5573fb648e6d
bridger <bdysonsm@utk.edu>
2016-11-15 16:56:52 -0500

    update script 180: vset the islandora_metadata_display to 'islandora_solr_metadata'

--- scripts/custom_scripts/180_modify_islandora_scholar_metadata_display.sh
+++ scripts/custom_scripts/180_modify_islandora_scholar_metadata_display.sh
@@ -9,3 +9,6 @@ drush vset islandora_scholar_use_standard_metadata_display 1
 
 echo "Updating islandora_solr's 'limit results to fields listed above' setting."
 drush vset islandora_solr_limit_result_fields 1
+
+echo "Updating islandora_solr's 'default metadata viewer' setting."
+drush vset islandora_metadata_display 'islandora_solr_metadata'
