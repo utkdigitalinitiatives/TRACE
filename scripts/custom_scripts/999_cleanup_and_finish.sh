8e754451a9964c62f26f4d30fe1399452490dc09
Don Richards <highlander411@hotmail.com>
2016-11-17 09:01:48 -0500

    Add drush/drupal update and comment formatting

--- scripts/custom_scripts/999_cleanup_and_finish.sh
+++ scripts/custom_scripts/999_cleanup_and_finish.sh
@@ -2,5 +2,17 @@
 
 # Add any cleanup Commands here
 
+# For root developers to use locate commands
 sudo updatedb
+
+# Drupal into maintenance mode
+drush vset --exact maintenance_mode 1 drush cache-clear all
+
+# Update Drupal
+yes | drush pm-update drupal
+
+# Drupal out of maintenance mode
+drush vset --exact maintenance_mode 0 drush cache-clear all
+
+# Clear drush's cache
 drush cc all
