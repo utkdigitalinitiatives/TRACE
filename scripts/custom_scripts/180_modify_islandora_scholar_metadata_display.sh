2a0c482850a028b9709957b4c7b85e8e7bc85e9c
bridger <bdysonsmith@gmail.com>
2016-11-18 13:55:17 -0500

    update custom_scripts/180_... to accommodate TRAC-374

--- scripts/custom_scripts/180_modify_islandora_scholar_metadata_display.sh
+++ scripts/custom_scripts/180_modify_islandora_scholar_metadata_display.sh
@@ -12,3 +12,11 @@ drush vset islandora_solr_limit_result_fields 1
 
 echo "Updating islandora_solr's 'default metadata viewer' setting."
 drush vset islandora_metadata_display 'islandora_solr_metadata'
+
+# cat the list of filters into the islandora_solr_base_filter variable
+if [ ! -f "$SHARED_DIR"/configs/islandora_solr_base_filter_list ]; then
+	echo "Missing the islandora_solr_base_filter_list file!"
+	break
+else
+	cat "$SHARED_DIR"/configs/islandora_solr_base_filter_list | drush vset islandora_solr_base_filter -
+fi
