dc1235892fcf6547964aa423edd68d59b90d3fd7
pc37utn <pc37@utk.edu>
2016-11-29 15:18:13 -0500

    remove can embargo perm in authuser

--- scripts/custom_scripts/030_drush_add_authUser_user.sh
+++ scripts/custom_scripts/030_drush_add_authUser_user.sh
@@ -21,7 +21,6 @@ declare -a AUTH_USER_PERMS=(
   "export islandora bookmarks"
   "share islandora bookmarks"
   "use islandora_bookmark"
-  "can embargo owned objects"
 )
 
 # iterate over the list of permissions and verify that they're added
