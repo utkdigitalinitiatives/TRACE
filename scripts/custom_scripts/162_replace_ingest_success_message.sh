aeb115faad07c546a885722baf2f7de4e249eece
cdeaneGit <cdeane@utk.edu>
2016-12-12 12:24:01 -0500

    TRAC-459 revise script to use aliases in file paths. And chown file copied to server.

--- scripts/custom_scripts/162_replace_ingest_success_message.sh
+++ scripts/custom_scripts/162_replace_ingest_success_message.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
-# This script replaces the file: /var/www/drupal/sites/all/modules/islandora/includes/ingest.form.inc 
-# to modify the function:  islandora_ingest_form_submit
-# to replace the message: t('"@label" (ID: @pid) has been ingested.', array('@label' => $object->label, '@pid' => $object->id)), 'status')
-# to the new message: :wqt('Success!')
+# This script replaces the file: $DRUPAL_HOME/sites/all/modules/islandora/includes/ingest.form.inc 
+# with the file: /vagrant/files/ingest.form.inc
+
+# Purpose:
+#  - modify the function:  islandora_ingest_form_submit
+#  - replace the message: t('"@label" (ID: @pid) has been ingested.', array('@label' => $object->label, '@pid' => $object->id)), 'status')
+#  - with the new message: t('Success!')
+
 # Per Trac-459 : Successful submission message
-# cdeane Dec 11, 2016
+# cdeane Dec 12, 2016
 
 
-# replace the file: ingest.form.inc"
-echo "Replacing the file: ingest.form.inc"
-sudo cp /vagrant/files/ingest.form.inc /var/www/drupal/sites/all/modules/islandora/includes/
+# replace the file: ingest.form.inc
+echo "Replacing the file: ingest.form.inc per TRAC-459"
+sudo cp /vagrant/files/ingest.form.inc $DRUPAL_HOME/sites/all/modules/islandora/includes/
 
 
 echo "Changing ownership of the file ingest.form.inc"
-sudo chown -hR vagrant:vagrant /var/www/drupal/sites/all/modules/islandora/includes/ingest.form.inc
+sudo chown -hR vagrant:vagrant $DRUPAL_HOME/sites/all/modules/islandora/includes/ingest.form.inc
 
 echo "End exectution of 162_replace_ingest_success_message.sh"
 
