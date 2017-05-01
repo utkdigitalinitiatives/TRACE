ba24d4861462a0d410720f24d92b121580265023
Mark Baggett <mbagget1@utk.edu>
2017-04-30 21:37:31 -0400

    Address TRAC-711.

--- scripts/modules.sh
+++ scripts/modules.sh
@@ -76,6 +76,10 @@ cd "$DRUPAL_HOME"/sites/all/modules/islandora_nested_collections || exit
 git config core.filemode false
 cd "$DRUPAL_HOME"/sites/all/modules || exit
 
+# clone Ashok Modi's module to extend XML Form Builder to Work with Field Panels and Field Panes
+cd "$DRUPAL_HOME"/sites/all/modules || exit
+git clone https://github.com/cherryhill/islandora_form_fieldpanel
+
 # Clone Tuque, BagItPHP, and Cite-Proc
 cd "$DRUPAL_HOME"/sites/all || exit
 if [ ! -d libraries ]; then
@@ -137,6 +141,7 @@ drush -y -u 1 en islandora_binary_object
 drush -y -u 1 en islandora_ingest_collections islandora_nested_collections
 drush -y -u 1 en rules_admin trace_ext_workflow
 drush -y -u 1 en islandora_binary_object_zip_importer
+drush -y -u 1 en islandora_form_fieldpanel
 
 cd "$DRUPAL_HOME"/sites/all/modules || exit
 
