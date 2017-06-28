2d6593141bb52dfd34e348394dd3205101b3c907
Robert Patrick Waltz (UTK) <rwaltz@utk.edu>
2017-06-28 10:07:20 -0400

    Changing the default location for xdebug information

--- scripts/custom_scripts/880_add_and_config_xdebug.sh
+++ scripts/custom_scripts/880_add_and_config_xdebug.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
+export DEBIAN_FRONTEND=noninteractive
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
+
 
 # install xdebug
-sudo apt-get install php5-xdebug;
+sudo apt-get install --quiet --assume-yes --force-yes php5-xdebug
 
-PHP_INI="/etc/php5/apache2/php.ini"
-
+#PHP_INI="/etc/php5/apache2/php.ini"
+PHP_INI="/etc/php5/apache2/conf.d/20-xdebug.ini" 
+IP_ADDRESS="192.168.160.1" 
 # add the following config settings to apache2's php.ini
 # use printf, yo, except for our 'then' because printf doesn't
 # like to have variables in its formatted output.
@@ -12,13 +21,13 @@ if grep -Fxq 'xdebug.idekey="TRACE-DEV"' $PHP_INI
 	then
 		echo "Already updated $PHP_INI"
 	else
-		printf "[Xdebug]\n" | sudo tee -a $PHP_INI
+		printf "[Xdebug]\n" | sudo tee $PHP_INI
 		printf "zend_extension=/usr/lib/php5/20121212/xdebug.so\n" | sudo tee -a $PHP_INI
 		printf "xdebug.remote_enable=1\n" | sudo tee -a $PHP_INI
-		printf "xdebug.remote_host=localhost\n" | sudo tee -a $PHP_INI
+		printf "xdebug.remote_host=${IP_ADDRESS}\n" | sudo tee -a $PHP_INI
 		printf "xdebug.remote_port=9000\n" | sudo tee -a $PHP_INI
 		printf "xdebug.idekey=\"TRACE-DEV\"\n" | sudo tee -a $PHP_INI
 fi
 
 # check it:
-sudo tail -n 10 $PHP_INI;
+#sudo tail -n 10 $PHP_INI;
