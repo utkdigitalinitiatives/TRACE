8be3d0006f7a436744e62791f2f421a73f9f644c
Robert Patrick Waltz (UTK) <rwaltz@utk.edu>
2017-07-10 17:35:00 -0400

    Address TRAC-774.
    refs TRAC-774. add a page for trace policy . populate it with polcy text from trace.tennessee.edu

--- scripts/custom_scripts/create_pages.php
+++ scripts/custom_scripts/create_pages.php
@@ -87,6 +87,96 @@ $about_body =  <<<ABOUT_BODY
 <p></p>
 ABOUT_BODY;
 
+
+$policy_body = <<<POLICY_BODY
+<div id="policies">
+<p>Trace, the Tennessee Research and Creative Exchange digital repository at the University of Tennessee, provides long-term access and preservation services for the intellectual and creative output of the university's academic, research, and administrative communities. The digital repository is open access, making content freely available, worldwide, under the following conditions and terms.</p>
+
+<h3>Organization</h3>
+<p>Trace is organized by communities - academic and other units at the University of Tennessee. Any school, academic department or program, research center or research institute at the University of Tennessee, as well as other administrative units that can assume responsibility for setting and administering policies, is eligible to form a community in the digital repository. Each community assigns a community manager who works with the University Libraries to support Trace.</p>
+<p>Communities develop, in consultation with the Trace administrator, policies addressing: 1) Items appropriate for a particular collection; 2) Electronic format acceptable for a particular collection; and 3) Guidelines for contributors.</p>
+<p>Trace is directed by the University Libraries Scholarly Communication department head. A Trace administrator oversees technical support and the addition of content. Campus stakeholders who serve on the Trace Advisory Group assist with the solicitation of new content and training of repository contributors.</p>
+
+<p>Communities:</p>
+<ul>
+  <li>Identify community managers who collaborate with repository staff</li>
+  <li>Develop community-specific policies to supplement general repository policies</li>
+  <li>Inform contributors about policies and procedures</li>
+  <li>Manage the deposit and description of content</li>
+  <li>Establish deposit workflows for collections</li>
+  <li>Notify repository staff of organizational changes affecting submissions</li>
+</ul>
+<p>University Libraries:</p>
+<ul>
+  <li>Retains content deposited</li>
+  <li>Preserves content using digital preservation standards and techniques</li>
+  <li>Provides access to repository content</li>
+  <li>Notifies communities of significant preservation actions, e.g. format migration</li>
+  <li>Provides a streaming server to host media files</li>
+</ul>
+<h3>Appropriate Submissions</h3>
+<p>Any content is appropriate if all applicable policies are followed (e.g., copyright), if the content can be posted using existing format types (i.e., technically feasible), and if the sponsoring unit decides it is appropriate. Posting bibliographic citations or abstracts alone cannot be accommodated without the full text of a work. Examples of appropriate Trace content include:</p>
+<ul>
+  <li>Published faculty research (as copyright permits)</li>
+  <li>Technical reports, working papers, conference publications</li>
+  <li>Research data in numeric or image formats</li>
+  <li>Theses &amp; Dissertations</li>
+  <li>Administrative records</li>
+  <li>Special collections digitized locally (letters, photographs, diaries)</li>
+  <li>Digital image collections</li>
+  <li>Instructional materials and courseware</li>
+  <li>Journal publishing platforms</li>
+  <li>Software</li>
+</ul>
+<p>SelectedWorks Author pages, Trace's option for University of Tennessee scholars to have a personal webpage that showcases their work, is currently available for faculty associated with UTK.</p>
+<p>Trace also supports peer-reviewed series and journals. If you are considering the repository for peer-reviewed content, visit <a href="http://www.trace.tennessee.edu/" title="Trace">www.trace.tennessee.edu</a> to determine if this is the right forum for your scholarship. Additionally, a conference or seminar series can be established within any department, center, or research unit participating in the repository. Creation of schedules and uploading of content is managed by the community manager of the sponsoring unit. If you are considering hosting a seminar series in the repository, contact
+  <script type="text/javascript">
+    //<![CDATA[
+        document.write("<a href='mailto:" + "trace" + "@" + "utk.edu" + "'>" + "trace" + "@" + "utk.edu" + "<\/a>")
+    //]]>
+</script><a href="mailto:trace@utk.edu">trace@utk.edu</a>.</p>
+
+<h3>Copyright and Author Agreements</h3>
+<p>Authors must own the copyright for work submitted to Trace. Authors grant the university a non-exclusive, perpetual right to use the digital assets for non-commercial use. Because authors retain the copyright for all content posted in the repository, they are free to reuse the content elsewhere.</p>
+<p>If a working paper is published in a journal - either in the same form or, more commonly, in revised form - many journals allow the working paper to continue to be openly accessible, especially when it is for educational/scholarly noncommercial use. While some journals require that the working paper be removed, others grant exceptions for institutional repositories, but must be asked.</p>
+<p>The author is responsible for negotiating the terms of publication agreements. The repository constitutes noncommercial use. Faculty who wish to include a reprint of a journal article in the repository should consult their agreement with the journal to see if posting in a repository is allowed. Authors who have transferred copyrights may seek permission of the copyright holder to publish it in the repository. The <a href="http://www.sherpa.ac.uk/projects/sherparomeo.html" title="SHERPA/RoMEO" target="_blank">SHERPA/RoMEO</a>  site provides information about publisher copyright policies.</p>
+<p>Unless otherwise noted, documents posted in Trace are the property of their respective authors. Authors who deposit material in the repository agree to the following:</p>
+
+<div class="indent">
+  <p>I hold the copyright to this document, or have been authorized by the copyright holder(s) to upload it for distribution, and agree to permit this document to be posted in Trace, and made available to the public in any format in perpetuity.</p>
+  <p>To the best of my knowledge, I warrant that the posting of the work does not infringe any copyright, nor violate any proprietary rights, nor contains any libelous matter, nor invade the privacy of any person or third party, nor otherwise violate Trace policies.</p>
+</div>
+
+<p>All permission requests should be directed to the individual authors or the stated holders of copyright. The University of Tennessee claims no rights over the content in Trace. For more information about copyright, please see the University Libraries Scholarly Communication Copyright pages at <a href="http://www.lib.utk.edu/scholar/copyright/" target="_blank">http://www.lib.utk.edu/scholar/copyright/</a>.</p>
+
+<h3>Author Review</h3>
+<p>Trace automatically creates a PDF from a Word or rtf document; in some cases the author should check the PDF once a paper has been uploaded to the system but before it is posted. The decision to have author review or not is the responsibility of each unit. The exception is peer-reviewed series and journals, where author review is essential.</p>
+
+<h3>Formats Supported and Preservation</h3>
+<p>Trace recommends the use of particular file types to increase the likelihood of safely migrating files into the future. Word and RTF files are converted to PDF when they are imported into the repository. For more information on preparing PDF versions of papers, see our file format and conversion guide.</p>
+<p>We recommend the following formats as most likely to migrate effectively:</p>
+<div class="indent">
+  <p>Images: JPEG, PNG, GIF, TIFF</p>
+  <p>Audio: AIFF, MP3 "unprotected" (DRM-free) AAC, WAVE</p>
+  <p>Video: MPEG, QuickTime</p>
+  <p>Data: XML, Comma-separated values</p>
+</div>
+<p>Other formats may be included for associated materials, but are less likely to be migrated when technology changes. Special arrangements for streaming video and for files larger than 100 MB should be made with the <a href="mailto:trace@utk.edu">Trace administrator</a>.</p>
+<p>The library is committed to providing access to and preserving all contents of the repository. When we migrate to new formats, we will change the format of a file, but not the information in it. For example, the Trace administrator will not update hyperlinks, other external references, or out-of-date information within a paper or supplementary file. If file formats cease to be supported as a result of changes in technology and the private sector, beyond the control of the University Libraries, files in these formats may no longer function on current computer equipment and hence, become unusable. If a new repository platform is chosen, all content will be migrated and every effort made to ensure that all content continues to be accessible.</p>
+<p>The commitment to provide persistent access to content in Trace applies only to material housed on servers maintained directly or under contract by the University Libraries. Links and access to content of any format referenced outside Trace cannot be guaranteed.</p>
+
+<h3>Withdrawal of Materials</h3>
+<p>Trace is meant to be a permanent scholarly record (working papers may be an exception). Once an item is deposited, a citation to it will always remain. Removing content is discouraged. However, authors may request the community manager to remove an item, except for peer-review series and journals, where removal is not allowed. No files will be removed by the Trace administrator or community managers without author notification. If a contributor leaves the University, the material will remain in Trace; upon request, new contact information will be added to the files.</p>
+
+<h3>Suppression of Materials</h3>
+<p>Trace is meant to provide open access to content. However, authors may request that the community manager restrict access according to community guidelines. The system is designed to enable communities to host subscription-based content.</p>
+</div>
+POLICY_BODY;
+
+
+
+
+
 $home_nid = create_named_page("home", "", $home_body);
 
 $submit_nid = create_named_page("submit", "Deposit Your Work", $submit_body);
@@ -94,6 +184,9 @@ add_menu_link_to_trace_navigation($submit_nid, "user-menu", "Submit", 9);
 
 $about_nid = create_named_page("about", "About", "$about_body");
 add_menu_link_to_trace_navigation($about_nid, "trace-navigation", "About", 0);
+
+#$policy_nid = create_named_page("policy", "Policy", "$policy_body");
+#add_menu_link_to_trace_navigation($policy_nid, "trace-navigation", "Policy", 5);
 
 $browse_nid = create_named_page("browse", "", "");
 add_menu_link_to_trace_navigation($browse_nid, "trace-navigation", "Browse Collections", 2);
