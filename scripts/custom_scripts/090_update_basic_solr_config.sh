017ef2d9df1eca94d58c69138fa5f14c51707587
bridger <bdysonsmith@gmail.com>
2017-07-26 10:25:19 -0400

    address TRAC-812 TRAC-813: correct file paths in sed commands

--- scripts/custom_scripts/090_update_basic_solr_config.sh
+++ scripts/custom_scripts/090_update_basic_solr_config.sh
@@ -15,17 +15,15 @@ else
 fi
 
 # Determine target OS and related settings
+# shellcheck disable=SC2046
 if [ -f "/etc/centos-release" ]; then
 	OS="centos"
-	APACHE_GROUP="apache"
 	TOMCAT_NAME="tomcat"
 elif [ -f "/etc/redhat-release" ]; then
 	OS="redhat"
-	APACHE_GROUP="apache"
 	TOMCAT_NAME="tomcat"
 elif [ $(grep -c 'Ubuntu' /etc/issue) -ne 0 ]; then
 	OS="ubuntu"
-	APACHE_GROUP="www-data"
 	TOMCAT_NAME="tomcat7"
 fi
 
@@ -33,14 +31,14 @@ fi
 case "$OS" in
 	centos)
 		echo "You're using Centos!"
-		FGS_INDEX_TARGET="/var/lib/"$TOMCAT_NAME"/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/index/FgsIndex/"
-		FGS_CONF_TARGET="/var/lib/"$TOMCAT_NAME"/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/"
+		FGS_INDEX_TARGET="/var/lib/$TOMCAT_NAME/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/index/FgsIndex/"
+		FGS_CONF_TARGET="/var/lib/$TOMCAT_NAME/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/"
 		SOLR_TARGET="/usr/local/solr/collection1/conf/"
 		echo "Updating Fedora GSearch"
 		# fix file paths in the new files
 		sed -i 's|/vhosts/fedora/solr|/usr/local/solr|g' utk-fedora-fedoragsearch-solr-config/fedoragsearch-transforms/index.properties
-		sed -i "s|/vhosts/fedora/tomcat/webapps|/var/lib/$TOMCAT_NAME/webapps|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-transforms/foxmlToSolr.xslt
-		sed -i "s|/vhosts/fedora/tomcat/webapps|/var/lib/$TOMCAT_NAME/webapps|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-transforms/islandora_transforms/*.xslt
+		sed -i "s|/vhosts/fedora/tomcat|/var/lib/$TOMCAT_NAME|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-transforms/foxmlToSolr.xslt
+		sed -i "s|/vhosts/fedora/tomcat|/var/lib/$TOMCAT_NAME|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-transforms/islandora_transforms/*.xslt
 		sed -i "7s|/var/lib/tomcat7|/var/lib/$TOMCAT_NAME|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-conf/fedoragsearch.properties
 		sed -i "32s|/var/lib/tomcat7|/var/lib/$TOMCAT_NAME|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-conf/fgsconfig-basic-configForIslandora.properties
 		sed -i "37s|/var/log/tomcat7|/var/log/$TOMCAT_NAME|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-conf/fgsconfig-basic-configForIslandora.properties
@@ -62,14 +60,14 @@ case "$OS" in
 		;;
 	ubuntu)
 		echo "You're using Ubuntu!"
-		FGS_INDEX_TARGET="/var/lib/"$TOMCAT_NAME"/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/index/FgsIndex/"
-		FGS_CONF_TARGET="/var/lib/"$TOMCAT_NAME"/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/"
+		FGS_INDEX_TARGET="/var/lib/$TOMCAT_NAME/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/index/FgsIndex/"
+		FGS_CONF_TARGET="/var/lib/$TOMCAT_NAME/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/"
 		SOLR_TARGET="/usr/local/solr/collection1/conf/"
 		echo "Updating Fedora GSearch"
 		# fix file paths in the new files
 		sed -i 's|/vhosts/fedora/solr|/usr/local/solr|g' utk-fedora-fedoragsearch-solr-config/fedoragsearch-transforms/index.properties
-		sed -i "s|/vhosts/fedora/tomcat|/var/lib/$TOMCAT_NAME/webapps|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-transforms/foxmlToSolr.xslt
-		sed -i "s|/vhosts/fedora/tomcat|/var/lib/$TOMCAT_NAME/webapps|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-transforms/islandora_transforms/*.xslt
+		sed -i "s|/vhosts/fedora/tomcat|/var/lib/$TOMCAT_NAME|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-transforms/foxmlToSolr.xslt
+		sed -i "s|/vhosts/fedora/tomcat|/var/lib/$TOMCAT_NAME|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-transforms/islandora_transforms/*.xslt
 		sed -i "7s|/var/lib/tomcat7|/var/lib/$TOMCAT_NAME|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-conf/fedoragsearch.properties
 		sed -i "32s|/var/lib/tomcat7|/var/lib/$TOMCAT_NAME|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-conf/fgsconfig-basic-configForIslandora.properties
 		sed -i "37s|/var/log/tomcat7|/var/log/$TOMCAT_NAME|g" utk-fedora-fedoragsearch-solr-config/fedoragsearch-conf/fgsconfig-basic-configForIslandora.properties
