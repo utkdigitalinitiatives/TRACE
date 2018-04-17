4f85c0f3f2782e34840535a1902e1c536d411fd9
pc37utn <pc37@utk.edu>
2018-04-17 16:55:37 -0400

    remove commented adding of old collection policy from coll-manager script

--- scripts/custom_scripts/055_drush_add_coll_manager_user.sh
+++ scripts/custom_scripts/055_drush_add_coll_manager_user.sh
@@ -57,6 +57,3 @@ drush_thesis_manager_role_perm_check
 ## assign graduate_thesis_manager_role to graduate_thesis_manager_user user
 drush -r "$DRUPAL_HOME" user-add-role 'thesis_manager_role' thesis_manager
 
-## add the collection manager policy to the Graduate Thesis collection
-# this is adding a datastream from vboxes/policies
-#curl -vv -u "fedoraAdmin:$FEDORAPASS" -X POST "http://localhost:8080/fedora/objects/utk.ir:td/datastreams/POLICY?controlGroup=M&dsLocation=http://dlwork.lib.utk.edu/vboxes/policies/collectionmanager.xml"
